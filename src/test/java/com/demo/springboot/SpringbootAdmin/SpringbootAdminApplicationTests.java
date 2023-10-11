03f341468fa63de8824bad8356abae41f72e711f
codewithsarthak102 <sarthakpandat781@gmail.com>
2023-10-11 21:58:06 +0530

    Initial Commit

---
+++ src/test/java/com/demo/springboot/SpringbootAdmin/SpringbootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springboot.SpringbootAdmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
